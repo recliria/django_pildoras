8be5e683fed301431ef24f54d956cc766a24823f
raul <cuestaliriaraul@gmail.com>
2024-07-18 10:55:59 -0400

    Postgresql connection settings

--- TiendaOnline/settings.py
+++ TiendaOnline/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = 'TiendaOnline.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "ArticulosClientes",
+        "USER": "postgres",
+        "PASSWORD": "P1stgr1sql*09408/",
+        "HOST": "localhost",
+        "PORT": 5432
     }
 }
 
