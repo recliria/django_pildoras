94a519cc07a5a6d5aeefede9a9405e13713f58a7
raul <cuestaliriaraul@gmail.com>
2024-07-22 12:26:41 -0400

    It was added str function in Articulos model

--- gestionPedidos/models.py
+++ gestionPedidos/models.py
@@ -15,6 +15,10 @@ class Articulos(models.Model):
     seccion = models.CharField(max_length=20)
     precio = models.IntegerField()
 
+    def __str__(self):
+
+        return "El artículo {}, pertenece a la sección {}, con un precio de {}".format(self.nombre,self.seccion,self.precio)
+
 class Pedidos(models.Model):
 
     numero = models.IntegerField()
