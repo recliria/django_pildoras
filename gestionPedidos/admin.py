dc9bf51fa4f66fe4f64d155bf29e9ce57d5919cd
raul <cuestaliriaraul@gmail.com>
2024-07-23 11:17:48 -0400

    Making changes in Admin views like adding filters

--- gestionPedidos/admin.py
+++ gestionPedidos/admin.py
@@ -2,6 +2,21 @@ from django.contrib import admin
 from gestionPedidos.models import Cliente,Articulo,Pedido
 # Register your models here.
 
-admin.site.register(Cliente)
-admin.site.register(Articulo)
-admin.site.register(Pedido)
+class ClienteAdmin(admin.ModelAdmin):
+
+    list_display = ("nombre","direccion","email","telefono")
+    search_fields = ("nombre","telefono")
+
+class ArticuloAdmin(admin.ModelAdmin):
+
+    list_filter = ("seccion",)
+
+class PedidoAdmin(admin.ModelAdmin):
+
+    list_display = ("numero","fecha")
+    list_filter = ("fecha",)
+    date_hierarchy = "fecha"
+
+admin.site.register(Cliente,ClienteAdmin)
+admin.site.register(Articulo,ArticuloAdmin)
+admin.site.register(Pedido,PedidoAdmin)
