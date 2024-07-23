0efe5ae0d0dc9de849558a47c8f5f1c0b72e6399
raul <cuestaliriaraul@gmail.com>
2024-07-23 03:28:28 -0400

    It was added the models in Admin

--- gestionPedidos/admin.py
+++ gestionPedidos/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from gestionPedidos.models import Cliente,Articulo,Pedido
 # Register your models here.
+
+admin.site.register(Cliente)
+admin.site.register(Articulo)
+admin.site.register(Pedido)
